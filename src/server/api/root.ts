2142aea15d853520979abc3b322b6d1c6fd127a7
_ 0ne <kouguchi3456@yahoo.co.jp>
2023-05-29 22:58:20 +0900

    create: custom hook for client run server fucntion

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -7,7 +7,7 @@ import { todoRouter } from "./routers/todo";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: todoRouter,
+  todo: todoRouter,
 });
 
 // export type definition of API
